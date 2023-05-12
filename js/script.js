7656ee827071a5d4f3ec57af7e38d1a1a9a77877
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-05-12 10:41:54 +0200

    Add function getMoveName & function displayResult

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 /**
- * getMoveName
+ * getMoveName 
  */
-
 function getMoveName(argMoveId){
-
+    console.log('wywołano funkcję getMoveName z argumentem ' + argMoveId);
+    if(argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu o id: ' + argMoveId)
+    }
 }
-
-
 
 /**
  * displayResult
  */
-
 function displayResult(argPlayerMove, argComputerMove){
-
-}
-
-/**
- * Interfejs gry
- */
-
+    console.log('wywolano funkcję displayResut z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+    if (argPlayerMove == 'papier' && argComputerMove == 'kamień'){
+        printMessage ('Wygrywasz!');
+    } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce'){
+        printMessage('Wygrywasz!');
+    } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+        printMessage('Wygrywasz!');
+    } else {
+        printMessage('Przegrywasz!');
+    }
+    printMessage('Zagrałem ' + argComputerMove + ' a Ty zagrałeś ' + argPlayerMove);
